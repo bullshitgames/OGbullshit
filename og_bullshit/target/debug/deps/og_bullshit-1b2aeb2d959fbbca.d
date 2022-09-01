4939c9d4aa8d1a742a4f379afdb512ad8f720004
Furkan <mfurkaner.mfe@gmail.com>
2022-09-01 09:44:06 +0300

    extension files

---
+++ og_bullshit/target/debug/deps/og_bullshit-1b2aeb2d959fbbca.d
@@ -0,0 +1,5 @@
+/Users/mfurkaner/Desktop/Projeler/Rust/OGbullshit/og_bullshit/target/debug/deps/og_bullshit-1b2aeb2d959fbbca.rmeta: src/main.rs
+
+/Users/mfurkaner/Desktop/Projeler/Rust/OGbullshit/og_bullshit/target/debug/deps/og_bullshit-1b2aeb2d959fbbca.d: src/main.rs
+
+src/main.rs:
---
+++ og_bullshit/target/debug/deps/og_bullshit-629b633f8650b4bd.d
@@ -0,0 +1,5 @@
+/Users/mfurkaner/Desktop/Projeler/Rust/OGbullshit/og_bullshit/target/debug/deps/og_bullshit-629b633f8650b4bd.rmeta: src/main.rs
+
+/Users/mfurkaner/Desktop/Projeler/Rust/OGbullshit/og_bullshit/target/debug/deps/og_bullshit-629b633f8650b4bd.d: src/main.rs
+
+src/main.rs:
