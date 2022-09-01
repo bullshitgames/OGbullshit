ad765739310596057a7bc4eba824201af4e35e45
Muhammet Furkan Er <furkan.er@siemens.com>
2022-09-01 12:40:47 +0300

    made a chat server

--- og_telnetserver/src/main.rs
+++ og_telnetserver/src/main.rs
@@ -1,27 +1,46 @@
 use tokio::net::TcpListener;    
-use tokio::io::{BufReader, AsyncBufReadExt, AsyncWriteExt};    
+use tokio::io::{BufReader, AsyncBufReadExt, AsyncWriteExt};   
+use tokio::sync::broadcast; 
+use std::net::SocketAddr; 
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
-
+    let (tx, _rx) = broadcast::channel::<(String, SocketAddr)>(10);   
 
     loop {
         
         let (mut socket, _addr) = listener.accept().await.unwrap();
 
-        let (reader_half, mut writer_half) = socket.split();
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move {
+            let (reader_half, mut writer_half) = socket.split();
     
-        let mut reader = BufReader::new(reader_half);
-        let mut line = String::new();
+            let mut reader = BufReader::new(reader_half);
+            let mut line = String::new();
+    
+            loop {
+                tokio::select!{
+                    read = reader.read_line(&mut line) => {
+                        if read.unwrap() == 0 { break; }
 
-        loop {
-            let bytes_read = reader.read_line(&mut line).await.unwrap();
+                        tx.send( (line.clone(), _addr) ).unwrap();
+                        line.clear();
+                    }
+                    msg = rx.recv() => {
+                        let (mut mes , other_addr) = msg.unwrap();
 
-            if bytes_read == 0 { break }
+                        if other_addr != _addr {
+                            mes = other_addr.ip().to_string() + ":" + &other_addr.port().to_string() + "> " + &mes;
+                            writer_half.write_all(mes.as_bytes()).await.unwrap();
+                        }
+                    }
+                }
+                
+            }
+        });
 
-            writer_half.write_all(line.as_bytes()).await.unwrap();
-            line.clear();
-        }
     }
 }
