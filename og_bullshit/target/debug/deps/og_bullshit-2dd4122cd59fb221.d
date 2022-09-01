61318ea77bb637149a706e3125521f0d85a0f91c
Furkan <mfurkaner.mfe@gmail.com>
2022-09-01 09:41:17 +0300

    clean build

---
+++ og_bullshit/target/debug/deps/og_bullshit-2dd4122cd59fb221.d
@@ -0,0 +1,5 @@
+/Users/mfurkaner/Desktop/Projeler/Rust/OGbullshit/og_bullshit/target/debug/deps/og_bullshit-2dd4122cd59fb221: src/main.rs
+
+/Users/mfurkaner/Desktop/Projeler/Rust/OGbullshit/og_bullshit/target/debug/deps/og_bullshit-2dd4122cd59fb221.d: src/main.rs
+
+src/main.rs:
---
+++ og_bullshit/target/debug/og_bullshit.d
@@ -0,0 +1,1 @@
+/Users/mfurkaner/Desktop/Projeler/Rust/OGbullshit/og_bullshit/target/debug/og_bullshit: /Users/mfurkaner/Desktop/Projeler/Rust/OGbullshit/og_bullshit/src/main.rs
